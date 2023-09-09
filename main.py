cf90704b74a689fafed6af7a18dbcb96890e6338
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2023-09-09 18:44:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.header("Movies Links")
+st.write("Ibomma website [ibomma] - (https://ww5.ibomma.link/telugu-movies/)")
+st.write("Movie rulz website [movierulz] - (https://ww26.watchmovierulz.to/)")
+st.write("Movie [Jailer](https://ww5.ibomma.link/te-a1cwn/jailer-2023-g5dbs-telugu-movie-watch-online.html)")
+st.write("Movie [Bro](https://ww5.ibomma.link/te-a1cwn/bro-2023-y6fdv-telugu-movie-watch-online.html)")
+st.write("Movie - [Miss shetty Mr polishetty](https://ww26.watchmovierulz.to/miss-shetty-mr-polishetty-2023-telugu/full-movie-watch-online-free-1-6811.html)")
