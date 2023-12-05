211ed34304e4e102160ab0f26dd32057df1130f9
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2023-12-05 16:44:13 +0530

    main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ st.write("Movie [Jailer](https://ww5.ibomma.link/te-a1cwn/jailer-2023-g5dbs-telu
 st.write("Movie [Bro](https://ww5.ibomma.link/te-a1cwn/bro-2023-y6fdv-telugu-movie-watch-online.html)")
 st.write("Movie - [Miss shetty Mr polishetty](https://ww26.watchmovierulz.to/miss-shetty-mr-polishetty-2023-telugu/full-movie-watch-online-free-1-6811.html)")
 st.write("Movie - [Bhagavath Kesari](https://leo.ibomma.day/d/Bhagavanth-Kesari-2023-sd7S-telugu-movie-watch-online.html)")
+st.write("Movie - [Bhola Shankar](https://bet.ibomma.link/te-a1cwn/bholaa-shankar-2023-g6uic-telugu-movie-watch-online.html)")
