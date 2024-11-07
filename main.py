ebc9c02e99aab1753a17b99479431d35bd2515f6
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2024-11-07 20:28:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.header("Movies Links")
 st.write("I bomma Link - [ibomma](https://osk.ibomma.wf/telugu-movies/)")
-st.write(" Lucky Bhaskar - [Lucky Bhaskar](https://www.5movierulz.io/lucky-baskhar-2024-telugu/movie-watch-online-free-3649.html)")
+st.write(" Lucky Baskhar - [Lucky Baskhar](https://www.5movierulz.io/lucky-baskhar-2024-telugu/movie-watch-online-free-3649.html)")
 st.write(" Movierulz - (https://www.5movierulz.io/)")
