8d7fee430e90ff7320181d8814f645a194fe226e
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2024-12-29 22:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.header("Movies Links")
-st.write("Bharateeyudu 2 -[Bharateeyudu 2](https://www.5movierulz.best/bharateeyudu-2-2024-telugu/movie-watch-online-free-2966.html321q)")
+st.write("Bharateeyudu 2 -[Bharateeyudu 2](https://www.5movierulz.best/bharateeyudu-2-2024-telugu/movie-watch-online-free-2966.html)")
 st.write("PUSHPA 2 - [PUSHPA](https://day.ibomma.observer/ca-tex4c/pushpa-the-rule-part-2-2024-r2tdf-telugu-movie-watch-online.html)")
 st.write("I bomma Link - [ibomma](https://osk.ibomma.wf/telugu-movies/)")
 st.write(" Lucky Baskhar - [Lucky Baskhar](https://www.5movierulz.io/lucky-baskhar-2024-telugu/movie-watch-online-free-3649.html)")
