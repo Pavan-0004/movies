920c7081c164ddcc22f2591f9edae0d85fa53ce2
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2025-01-13 21:56:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ st.write("PUSHPA 2 - [PUSHPA](https://day.ibomma.observer/ca-tex4c/pushpa-the-ru
 st.write("I bomma Link - [ibomma](https://osk.ibomma.wf/telugu-movies/)")
 st.write(" Lucky Baskhar - [Lucky Baskhar](https://www.5movierulz.io/lucky-baskhar-2024-telugu/movie-watch-online-free-3649.html)")
 st.write(" Movierulz - (https://www.5movierulz.rip/)")
-st.html(<iframe src="https://www.youtube.com" width="500" height="300"></iframe>)
+st.html("<iframe src="https://www.youtube.com" width="500" height="300"></iframe>")
