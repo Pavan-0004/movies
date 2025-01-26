e250b1a83dfc8fe72b3124c64d3c7575843de6a9
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2025-01-26 23:20:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ st.write(" Lucky Baskhar - [Lucky Baskhar](https://www.5movierulz.io/lucky-baskh
 st.write(" Movierulz - (https://www.5movierulz.rip/)")
 
 #html_string = "<div style="padding:42.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1050532555?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Sankranthiki Vasthunam"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>"
-st.markdown("<div style="padding:42.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1050532555?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Sankranthiki Vasthunam"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>"
-, unsafe_allow_html=True)
+st.markdown(
+    '<div style="padding:42.5% 0 0 0;position:relative;">'
+    '<iframe src="https://player.vimeo.com/video/1050532555?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" '
+    'frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" '
+    'style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Sankranthiki Vasthunam"></iframe></div>'
+    '<script src="https://player.vimeo.com/api/player.js"></script>',
+    unsafe_allow_html=True
+)
 
 #components.iframe("https://www.youtube.com", width=800, height=600) 
 #components.iframe("https://www.5movierulz.rip/daaku-maharaaj-2025-telugu/movie-watch-online-free-4064.html", width=800, height=600) 
