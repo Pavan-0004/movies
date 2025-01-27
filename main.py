f63ded1f5f47716c3a256fb5da77ac96b0bc636c
Mittapalli Pavan <118098180+Pavan-0004@users.noreply.github.com>
2025-01-27 21:10:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 #import streamlit.components.v1 as components
-***
+'''
 st.header("Movies Links")
 st.write("Bharateeyudu 2 -[Bharateeyudu 2](https://www.5movierulz.best/bharateeyudu-2-2024-telugu/movie-watch-online-free-2966.html)")
 st.write("PUSHPA 2 - [PUSHPA](https://day.ibomma.observer/ca-tex4c/pushpa-the-rule-part-2-2024-r2tdf-telugu-movie-watch-online.html)")
 st.write("I bomma Link - [ibomma](https://osk.ibomma.wf/telugu-movies/)")
 st.write(" Lucky Baskhar - [Lucky Baskhar](https://www.5movierulz.io/lucky-baskhar-2024-telugu/movie-watch-online-free-3649.html)")
 st.write(" Movierulz - (https://www.5movierulz.rip/)")
-***
+'''
 #html_string = "<div style="padding:42.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1050532555?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Sankranthiki Vasthunam"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>"
 st.markdown(
     '<div style="padding:42.5% 0 0 0;position:relative;">'
